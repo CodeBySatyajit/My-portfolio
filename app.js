599a4ceb37869648e70eb7dbecd187549c8b0b16
Satyajit <satyjeetgaikwad39@gmail.com>
2025-10-01 16:10:16 +0530

    add comments on js file

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const icon = document.querySelector(".bars-icon");
 const ul = document.querySelector("ul");
 const a = document.querySelectorAll("a");
 
+// add function for click on bar icon
+
 icon.addEventListener("click", () => {
   ul.classList.toggle("showData");
+
+  // for changing the icon of bar when open and close the ul
 
   if (ul.className == "showData") {
     document.getElementById("bar").className = "fa-solid fa-xmark";
@@ -12,12 +16,16 @@ icon.addEventListener("click", () => {
   }
 });
 
+// when the user click on any link of ul then ul will be closed
+
 a.forEach((anchor) => {
   anchor.addEventListener("click", () => {
     ul.classList.remove("showData");
     document.getElementById("bar").className = "fa-solid fa-bars";
   });
 });
+
+// Typing information animation
 
 var typed = new Typed("#element", {
   strings: ["Web Developer", "App Developer", "Software Developer"],
